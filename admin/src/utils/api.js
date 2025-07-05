61caf0d4e84c98dbd5aa681f3a445c0f60e4d054
DynamicAdi <adarshpanditdev@gmail.com>
2025-07-05 14:28:22 +0530

    admin modify

--- admin/src/utils/api.js
+++ admin/src/utils/api.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-const BASE_URL = import.meta.env.VITE_BASE_URL || "http://localhost:8000/api"; 
+const BASE_URL = import.meta.env.VITE_BASE_URL; 
 
 export const fetchDataFromApi=async(url)=>{
     try{
